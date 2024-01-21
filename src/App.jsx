bb31b4bbcfc9cdd459fb2372e907ad5f78a4e60b
Danko Kralski <d.kralski@student.fontys.nl>
2024-01-21 09:49:55 +0100

    Channel bar and a ContentContainer for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import ChannelBar from './components/Channel';
+import ContentContainer from './components/ContentContainer';
 import SideBar from './components/SideBar';
 
 function App() {
   return (
-    <>
+    <div className='flex'>
       <SideBar />
-    </>
+      <ChannelBar />
+      <ContentContainer />
+    </div>
   );
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ const SideBar = () => {
   return (
     <div
       className='fixed top-0 left-0 h-screen w-16 flex flex-col
-                  bg-fuchsia-900 dark:bg-gray-900 shadow-lg'
+                  bg-fuchsia-900'
     >
       <SideBarIcon icon={<FaFire size='28' />} popText={'fire'} />
       <hr className='sidebar-hr' />
