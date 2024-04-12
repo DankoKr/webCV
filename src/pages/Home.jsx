22d8662741e29a23ecd4c128d0fb40400d31d28f
DankoKr <dankokralski776@gmail.com>
2024-04-12 08:20:44 +0200

    fixed import of image on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,46 @@
-import './Home.scss';
-import { useEffect, useState } from 'react';
-import Loader from 'react-loaders';
-import AnimatedLetters from '../components/AnimatedLetters';
-import Programmer from '../assets/images/Programmer.png';
-import CV from '../assets/CV intern.pdf';
+import "./Home.scss";
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../components/AnimatedLetters";
+import Programmer from "../assets/images/programmer.png";
+import CV from "../assets/CV intern.pdf";
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate');
+  const [letterClass, setLetterClass] = useState("text-animate");
 
-  const nameArray = [' ', 'D', 'a', 'n', 'k', 'o'];
+  const nameArray = [" ", "D", "a", "n", "k", "o"];
   const jobArray = [
-    's',
-    'o',
-    'f',
-    't',
-    'w',
-    'a',
-    'r',
-    'e',
-    ' ',
-    'd',
-    'e',
-    'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
-    '.',
+    "s",
+    "o",
+    "f",
+    "t",
+    "w",
+    "a",
+    "r",
+    "e",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".",
   ];
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover');
+      setLetterClass("text-animate-hover");
     }, 4000);
   }, []);
 
   return (
     <>
-      <div className='container home-page'>
-        <div className='text-zone'>
+      <div className="container home-page">
+        <div className="text-zone">
           <h1>
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
@@ -60,16 +60,16 @@ const Home = () => {
             />
           </h1>
           <h2>Full Stack Developer</h2>
-          <a href={CV} className='flat-button'>
+          <a href={CV} className="flat-button">
             CHECK CV
           </a>
         </div>
-        <div className='programmer-zone'>
-          <img className='programmer' src={Programmer} />
+        <div className="programmer-zone">
+          <img className="programmer" src={Programmer} />
         </div>
       </div>
 
-      <Loader type='pacman' />
+      <Loader type="pacman" />
     </>
   );
 };
